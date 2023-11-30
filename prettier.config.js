6673cd879e8a6c77ac4d008c641901007a9c8db4
Puskar-Roy <puskarroy600@gmail.com>
2023-11-30 23:29:06 +0530

    Add : Eslint And Prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  endOfLine: "auto",
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  semi: true,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Working");
+import express, { Express, Request, Response, NextFunction } from 'express';
+import * as dotenv from 'dotenv';
+const app: Express = express();
+
+dotenv.config();
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Is Running on Port ${process.env.PORT}`);
+});
